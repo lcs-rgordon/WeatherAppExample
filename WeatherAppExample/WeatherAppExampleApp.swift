8ac1ed8aded7414b29d0cd1ff53a8fb91109e717
Russell Gordon <rgordon@lcs.on.ca>
2023-11-23 13:05:23 -0500

    Initial Commit

---
+++ WeatherAppExample/WeatherAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppExampleApp.swift
+//  WeatherAppExample
+//
+//  Created by Russell Gordon on 2023-11-23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
